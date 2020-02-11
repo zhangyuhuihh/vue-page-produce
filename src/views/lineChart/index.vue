6d69c9b72b85761832632d58092c2626321dd386
shine-lzx <1597395958@qq.com>
2020-02-11 12:43:53 +0800

    build linechart file

---
+++ src/views/lineChart/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="shyContainer">折线图</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lineChartData: []
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.shyContainer {
+  width: 300px;
+  height: 300px;
+}
+</style>
