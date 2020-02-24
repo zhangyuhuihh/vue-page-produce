f76a7bf19856de6bc7a8c0f12cabe60dda91d9f1
shiyunchao <1368397499@qq.com>
2020-02-24 10:28:47 +0800

    柱状图

---
+++ src/views/partComponent/json_histogram.js
@@ -0,0 +1,40 @@
+const jsonEchart = {
+    ForTestComponentThree: [
+        {
+            legend: '销量',
+            detail: [
+                {
+                    key:'1',
+                    value:'10'
+                },
+                {
+                    key:'2',
+                    value:'20'
+                },
+                {
+                    key:'3',
+                    value:'30'
+                },
+            ]
+        }
+        // {
+        //     legend: '销量2',
+        //     detail: [
+        //         {
+        //             key:'1',
+        //             value:'11'
+        //         },
+        //         {
+        //             key:'2',
+        //             value:'22'
+        //         },
+        //         {
+        //             key:'3',
+        //             value:'32'
+        //         },
+        //     ]
+        // },
+    ]
+  }
+  
+  export default jsonEchart
